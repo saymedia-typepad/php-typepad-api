453cf6638c0263619e52e7cabc91c69444bf2841
Kevin Shay <kshay@sixapart.com>
2010-08-06 13:01:45 -0400

    Regenerated docs

--- lib/TypePad.php
+++ lib/TypePad.php
@@ -22,6 +22,7 @@ require_once('libcurlemu/libcurlemu.inc.php');
  * Object class allowing interaction with the TypePad API.
  *
  * @package TypePad
+ * @subpackage TypePad
  */
 class TypePad {
     
