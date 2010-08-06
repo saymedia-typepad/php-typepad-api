a4f63841010a5e2b1c939dc759f128cd28ed3d7f
Finn Smith <finn@timeghost.net>
2010-08-06 12:37:34 -0400

    Clean up whitespace/EOL.

--- site/config.original.php
+++ site/config.original.php
@@ -38,7 +38,7 @@ define ('TP_GENERAL_PURPOSE_KEY', '');
 define ('TP_GENERAL_PURPOSE_SECRET', '');
 /**
  * The type of access that this application will request from TypePad on behalf
- * of a user who signs up for it. 'app_full' will give the user access to 
+ * of a user who signs up for it. 'app_full' will give the user access to
  * content owned by the application; 'typepad_full' will give the user access
  * to TypePad content.
  */
@@ -46,7 +46,7 @@ define ('TP_ACCESS_TYPE', 'app_full');
 
 /**
  * The default URL to which TypePad should redirect the browser after login
- * or logout. 
+ * or logout.
  */
 define ('TP_RETURN_URL', 'http://www.example.com/');
 /**
@@ -74,11 +74,11 @@ define ('TP_DB_PASSWORD', '');
  */
 define ('TP_DB_NAME', 'typepad');
 /**
- * The location of the non-secure TypePad API endpoints. 
+ * The location of the non-secure TypePad API endpoints.
  */
 define ('TP_API_BASE', 'http://api.typepad.com');
 /**
- * The location of the secure TypePad API endpoints. 
+ * The location of the secure TypePad API endpoints.
  */
 define ('TP_API_BASE_SECURE', 'https://api.typepad.com');
 /**
--- site/index.php
+++ site/index.php
@@ -12,8 +12,8 @@ $tp = new TypePad();
 <head>
 <?php
 
-// This function is provided for use on sites that allow login via TypePad. 
-// it writes a <script> tag to the page that calls in a script file from 
+// This function is provided for use on sites that allow login via TypePad.
+// it writes a <script> tag to the page that calls in a script file from
 // typepad.com, allowing TypePad to recognize that the user is logged in
 // to TypePad and, if so, provide the user's information to your PHP application.
 // See http://www.typepad.com/services/apidocs/authentication for more details,
@@ -31,7 +31,7 @@ $tp->openBatch();
 
 // Any calls to API endpoint functions that come after an openBatch()
 // will not call the API yet, but will add them to the batch; when you've
-// queued all the subrequests you need to render the page, call 
+// queued all the subrequests you need to render the page, call
 // $tp->runBatch().
 
 // Note that we don't need to wrap this in a try/catch block, because
@@ -48,28 +48,28 @@ $user = $tp->users->get('@self');
 <?php
 try {
 
-	// This will throw a TPException if any of the batch's subrequests 
-	// returned a non-success HTTP status. In this case, since we made a
-	// request for the '@self' user, which will only succeed if the user is
-	// logged in, we want to catch a 404 Not Found response and present 
-	// the user with the option to log in.
-	$tp->runBatch();
-	print "<p>Welcome, " . $user->displayName . "!</p>";
-	print '<p><a href="logout.php">Log out</a></p>';
+    // This will throw a TPException if any of the batch's subrequests
+    // returned a non-success HTTP status. In this case, since we made a
+    // request for the '@self' user, which will only succeed if the user is
+    // logged in, we want to catch a 404 Not Found response and present
+    // the user with the option to log in.
+    $tp->runBatch();
+    print "<p>Welcome, " . $user->displayName . "!</p>";
+    print '<p><a href="logout.php">Log out</a></p>';
 
 } catch (TPException $e) {
 
-	if ($e->getCode() == 404) {
-	    print <<<HTML
+    if ($e->getCode() == 404) {
+        print <<<HTML
 You are not logged in. <a href="login.php">Click here</a> to log in.
 HTML;
-	} else {
-		// We're not expecting an error other than a 404, so just tell
-		// the user about it.
-		print "An error occurred: "
-			. $e->getCode() .' '. $e->getMessage()
-			. " (request was " . $e->getRequest()->getUri() . ")";
-	}
+    } else {
+        // We're not expecting an error other than a 404, so just tell
+        // the user about it.
+        print "An error occurred: "
+            . $e->getCode() .' '. $e->getMessage()
+            . " (request was " . $e->getRequest()->getUri() . ")";
+    }
 }
 
 ?>
